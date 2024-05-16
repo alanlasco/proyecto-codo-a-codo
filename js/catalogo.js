f90d47a8d23556abbe57709eb4f7c17fc2b6ac00
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-05-16 18:17:38 -0300

    agregar href de los juegos

--- js/catalogo.js
+++ js/catalogo.js
@@ -7,42 +7,42 @@ const catalogo = [
   {
     path: "./img/ac3.jpeg",
     alt: "Assassin's Creed III",
-    href: "",
+    href: "https://www.ubisoft.com/es-mx/game/assassins-creed/assassins-creed-3-remastered",
   },
   {
     path: "./img/ac4.jpg",
     alt: "Assassin's Creed IV",
-    href: "",
+    href: "https://store.ubisoft.com/ofertas/assassins-creed-iv-black-flag-gold-edition/575ffdb4a3be1633568b4e92.html?lang=es_AR&lang=la_AR",
   },
   {
     path: "./img/acmirage.jpg",
     alt: "Assassin's Creed Mirage",
-    href: "",
+    href: "https://store.ubisoft.com/ofertas/assassins-creed-mirage/62ea4f9709372571f2736a72.html?lang=es_AR&lang=la_AR",
   },
   {
     path: "./img/acodyssey.png",
     alt: "Assassin's Creed Odyssey",
-    href: "",
+    href: "https://store.ubisoft.com/ofertas/assassins-creed-odyssey---ultimate-edition/5afda8aa6b54a4271407a877.html?lang=es_AR&lang=la_AR",
   },
   {
     path: "./img/acrevelations.jpg",
     alt: "Assassin's Creed Revelations",
-    href: "",
+    href: "https://www.ubisoft.com/es-mx/game/assassins-creed/revelations",
   },
   {
     path: "./img/acsyndicate.jpg",
     alt: "Assassin's Creed Syndicate",
-    href: "",
+    href: "https://www.ubisoft.com/es-mx/game/assassins-creed/syndicate",
   },
   {
     path: "./img/acunity.jpg",
     alt: "Assassin's Creed Unity",
-    href: "",
+    href: "https://store.ubisoft.com/ofertas/assassins-creed-unity/575ffdb7a3be1633568b4ed8.html?lang=es_AR&lang=la_AR",
   },
   {
     path: "./img/acvalhalla.png",
     alt: "Assassin's Creed Valhalla",
-    href: "",
+    href: "https://www.ubisoft.com/es-mx/game/assassins-creed/valhalla",
   },
 ];
 //simular useState
@@ -73,8 +73,8 @@ document.querySelector(".button").addEventListener("click", () => {
       const html = `
       <div class="carta">
         <article class=""><img src="${juego.path}"  alt="${juego.alt}"></article>
-        <div class="epigrafe"><a class="enlace"
-            href="${juego.href}">${juego.alt}</a>
+        <div class="epigrafe"><a class="enlace" target="_blank"
+            href="${juego.href}" >${juego.alt}</a>
         </div>
       </div>
 `;
