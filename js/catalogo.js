48580b32534f895188e2ef8b50440c21307b2c25
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-06-29 17:50:12 -0300

    integracion de fetch

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,57 +1,10 @@
-const catalogo = [
-  {
-    path: "./img/ac2.jpg",
-    alt: "Assassin's Creed II",
-    href: "",
-  },
-  {
-    path: "./img/ac3.jpeg",
-    alt: "Assassin's Creed III",
-    href: "https://www.ubisoft.com/es-mx/game/assassins-creed/assassins-creed-3-remastered",
-  },
-  {
-    path: "./img/ac4.jpg",
-    alt: "Assassin's Creed IV",
-    href: "https://store.ubisoft.com/ofertas/assassins-creed-iv-black-flag-gold-edition/575ffdb4a3be1633568b4e92.html?lang=es_AR&lang=la_AR",
-  },
-  {
-    path: "./img/acmirage.jpg",
-    alt: "Assassin's Creed Mirage",
-    href: "https://store.ubisoft.com/ofertas/assassins-creed-mirage/62ea4f9709372571f2736a72.html?lang=es_AR&lang=la_AR",
-  },
-  {
-    path: "./img/acodyssey.png",
-    alt: "Assassin's Creed Odyssey",
-    href: "https://store.ubisoft.com/ofertas/assassins-creed-odyssey---ultimate-edition/5afda8aa6b54a4271407a877.html?lang=es_AR&lang=la_AR",
-  },
-  {
-    path: "./img/acrevelations.jpg",
-    alt: "Assassin's Creed Revelations",
-    href: "https://www.ubisoft.com/es-mx/game/assassins-creed/revelations",
-  },
-  {
-    path: "./img/acsyndicate.jpg",
-    alt: "Assassin's Creed Syndicate",
-    href: "https://www.ubisoft.com/es-mx/game/assassins-creed/syndicate",
-  },
-  {
-    path: "./img/acunity.jpg",
-    alt: "Assassin's Creed Unity",
-    href: "https://store.ubisoft.com/ofertas/assassins-creed-unity/575ffdb7a3be1633568b4ed8.html?lang=es_AR&lang=la_AR",
-  },
-  {
-    path: "./img/acvalhalla.png",
-    alt: "Assassin's Creed Valhalla",
-    href: "https://www.ubisoft.com/es-mx/game/assassins-creed/valhalla",
-  },
-];
-//simular useState
+// Simular useState
 function useState(initialValue) {
   let state = initialValue;
   const getState = () => state;
   const setState = (newValue) => {
     state = newValue;
-    render(); //actualizar el botón
+    render(); // Actualizar el botón
   };
   return [getState, setState];
 }
@@ -59,34 +12,52 @@ function useState(initialValue) {
 // Variables para mantener el estado
 let [name, setName] = useState("Todos los juegos");
 let ok = false;
+let catalogo = [];
+
+// Renderizar el botón con el nombre actual
 function render() {
   const button = document.querySelector(".button");
   button.textContent = name();
 }
 
-const todos = document.querySelector(".todos");
+// Función para obtener los datos de la API
+const fetchCatalogo = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/juegos");
+    const data = await response.json();
+    catalogo = data;
+    renderCatalogo();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
-//evento de clic del botón
+// Función para renderizar el catálogo
+const renderCatalogo = () => {
+  const todos = document.querySelector(".todos");
+  todos.innerHTML = ""; // Limpiar contenido existente
+  catalogo.forEach((juego) => {
+    const html = `
+      <div class="carta">
+        <article class=""><img src="${juego.imagen_juego}" alt="${juego.nombre_juego}"></article>
+        <div class="epigrafe"><a class="enlace" target="_blank" href="${juego.url_juego}">${juego.nombre_juego}</a></div>
+      </div>
+    `;
+    todos.innerHTML += html;
+  });
+};
+
+// Evento de clic del botón
 document.querySelector(".button").addEventListener("click", () => {
   if (!ok) {
-    catalogo.forEach((juego) => {
-      const html = `
-      <div class="carta">
-        <article class=""><img src="${juego.path}"  alt="${juego.alt}"></article>
-        <div class="epigrafe"><a class="enlace" target="_blank"
-            href="${juego.href}" >${juego.alt}</a>
-        </div>
-      </div>
-`;
-
-      todos.innerHTML += html;
-    });
+    fetchCatalogo();
     ok = true;
   }
-  // Alternar el nombre del botón
+
+  // Alternar el nombre del botón y la visibilidad del catálogo
+  const todos = document.querySelector(".todos");
   if (name() === "Todos los juegos") {
     todos.style.display = "flex";
-
     setName("Ver menos");
   } else {
     todos.style.display = "none";
